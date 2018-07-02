3117840948c9c9a65749884f2e3849fd377d9a58
Jakub <jakub94@gmx.de>
2018-07-02 16:04:50 +0200

    Add css for chat page

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -20,14 +20,14 @@ describe('generateMessage', () => {
 });
 
 
-describe('generateLocationMessage', () => {
+describe('generateLocationMessage', () => { 
   it("should generate correct location object", () => {
 
     var from = 'Debra';
     var latitude = 45;
     var longitude = 11;
     var url = 'https://www.google.com/maps?q=45,11';
-    var message = generateLocationMessage(from, latitude, longitude);  
+    var message = generateLocationMessage(from, latitude, longitude);
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({
